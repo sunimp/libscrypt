e7c90619f956ba8acd53753ec8b383bc942e4ab3
杨光 <yangguang@webull.com>
2022-04-29 14:38:24 +0800

    fix(podfile): misc

--- Example/Pods/Target Support Files/libscrypt/libscrypt-umbrella.h
+++ Example/Pods/Target Support Files/libscrypt/libscrypt-umbrella.h
@@ -10,6 +10,12 @@
 #endif
 #endif
 
+#import "b64.h"
+#import "crypto_scrypt-hexconvert.h"
+#import "libscrypt.h"
+#import "sha256.h"
+#import "slowequals.h"
+#import "sysendian.h"
 
 FOUNDATION_EXPORT double libscryptVersionNumber;
 FOUNDATION_EXPORT const unsigned char libscryptVersionString[];
--- Example/libscrypt/ViewController.swift
+++ Example/libscrypt/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import libscrypt
 
 class ViewController: UIViewController {
 
