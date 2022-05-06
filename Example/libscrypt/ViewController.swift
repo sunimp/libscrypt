4742dedd449f219a8ac835e8835daaabe94e6361
杨光 <yangguang@webull.com>
2022-05-06 11:02:59 +0800

    fix(example): add caller

--- Example/libscrypt/ViewController.swift
+++ Example/libscrypt/ViewController.swift
@@ -13,7 +13,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let scrypt = Scrypt(password: "123456", salt: "Sun", n: 262144, r: 8, p: 1)
+        print(scrypt.encrypt())
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +26,55 @@ class ViewController: UIViewController {
 
 }
 
+class Scrypt {
+    private let password: String
+    private let salt: String
+    private let n: Int
+    private let r: Int
+    private let p: Int
+    private let dklen = 32
+    
+    // Param: password and salt should be in hex format.
+    init(password: String, salt: String, n: Int, r: Int, p: Int) {
+        self.password = password
+        self.salt = salt
+        self.n = n
+        self.r = r
+        self.p = p
+    }
+    
+    func encrypt() -> String {
+        
+        
+        var passwordBytes: [UInt8] = []
+        password.data(using: .utf8)?.withUnsafeBytes { (body: UnsafeRawBufferPointer) in
+            if let bytes = body.baseAddress?.assumingMemoryBound(to: UInt8.self).pointee {
+                passwordBytes = [bytes]
+            }
+        }
+        var saltBytes: [UInt8] = []
+        salt.data(using: .utf8)?.withUnsafeBytes { (body: UnsafeRawBufferPointer) in
+            if let bytes = body.baseAddress?.assumingMemoryBound(to: UInt8.self).pointee {
+                saltBytes = [bytes]
+            }
+        }
+        
+        var data = Data(count: dklen)
+        data.withUnsafeMutableBytes { (body: UnsafeMutableRawBufferPointer) -> Void in
+            
+            let bytes = body.baseAddress?.assumingMemoryBound(to: UInt8.self)
+            libscrypt_scrypt(
+                passwordBytes,
+                passwordBytes.count,
+                saltBytes,
+                saltBytes.count,
+                UInt64(n),
+                UInt32(r),
+                UInt32(p),
+                bytes,
+                dklen
+            )
+        }
+        return String(data: data, encoding: .utf8) ?? ""
+    }
+}
